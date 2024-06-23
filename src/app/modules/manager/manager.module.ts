2fde3b398c99446a7058438e3ca7853171cc716a
System Administrator <root@Satayenders-MacBook-Air.local>
2024-06-23 12:02:17 +0530

    removed extra component

--- src/app/modules/manager/manager.module.ts
+++ src/app/modules/manager/manager.module.ts
@@ -1,31 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ManagerRoutingModule } from './manager-routing.module';
-import { TaskListComponent } from './task-list/task-list.component';
+import {ManagerRoutingModule} from './manager-routing.module';
+import {TaskListComponent} from './task-list/task-list.component';
 import {SharedModule} from "../../shared/modules/shared/shared.module";
-import { AddTaskComponent } from './add-task/add-task.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
+import {AddTaskComponent} from './add-task/add-task.component';
+import {HeaderComponent} from './header/header.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatSelectModule} from "@angular/material/select";
 
 
 @NgModule({
-  declarations: [
-    TaskListComponent,
-    AddTaskComponent,
-    HeaderComponent,
-    SidebarComponent,
-    FooterComponent,
-  ],
-  imports: [
-    CommonModule,
-    ManagerRoutingModule,
-    SharedModule,
-    MatIconModule,
-    MatSelectModule,
-  ]
+    declarations: [
+        TaskListComponent,
+        AddTaskComponent,
+        HeaderComponent,
+
+    ],
+    imports: [
+        CommonModule,
+        ManagerRoutingModule,
+        SharedModule,
+        MatIconModule,
+        MatSelectModule,
+    ]
 })
-export class ManagerModule { }
+export class ManagerModule {
+}
